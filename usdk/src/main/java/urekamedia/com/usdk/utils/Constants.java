48dfe7c8db47bea2670402b2b0a67d1b27d9f8e8
quanghuy.phung <quanghuy.phung@urekamedia.vn>
2022-10-19 11:16:29 +0700

    update api

--- usdk/src/main/java/urekamedia/com/usdk/utils/Constants.java
+++ usdk/src/main/java/urekamedia/com/usdk/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String BANNER_URL = "http://banner.tin8.co/utivi";
 
     public static final String API_ROUTE_AD_CONFIG = "/uconnect/api/v1/getConfig";
-    public static final String API_ROUTE_AD_BANNER = "/api/getBannerBeatX";
+    public static final String API_ROUTE_AD_BANNER = "/api/getBannerPlayX";
     public static final String API_ROUTE_GET_LUCKYDRAW = "/api/getluckydraw";
     public static final String API_ROUTE_BANNER = "";
     public static final String API_ROUTE_AD_VIDEO = "/utivi/getVideo";
